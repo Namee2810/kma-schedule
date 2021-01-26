be25449b030868b601cd7cee5ca2edd05d4f3eea
Namee2810 <62344286+Namee2810@users.noreply.github.com>
2021-01-26 23:33:31 +0700

    .

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import store from 'store';
 import App from './App';
 import './index.css';
-import "./registerServiceWorker";
+import register from "./registerServiceWorker";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -20,3 +20,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+register()
